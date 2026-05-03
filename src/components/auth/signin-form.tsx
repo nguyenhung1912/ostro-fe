421e0e28c62486fab4c58c85e133112aacb1e611
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-03 13:54:29 +0700

    feat: implement sign-in functionality in SignInForm component

--- src/components/auth/signin-form.tsx
+++ src/components/auth/signin-form.tsx
@@ -6,6 +6,8 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Label } from "../ui/label";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useNavigate } from "react-router";
 
 const signInSchema = z.object({
   username: z.string().min(3, "Tên đăng nhập phải có ít nhất 3 ký tự"),
@@ -18,6 +20,8 @@ export function SignInForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const { signIn } = useAuthStore();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -27,7 +31,9 @@ export function SignInForm({
   });
 
   const onSubmit = async (data: SignInFormValues) => {
-    // call API sign in
+    const { username, password } = data;
+    await signIn(username, password);
+    navigate("/");
   };
 
   return (
