9893610c054822195bd838da737d8d8a320a6bf6
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-04 00:47:03 +0700

    refactor: enhance logout functionality with error handling

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,18 +1,23 @@
 import { useAuthStore } from "@/stores/useAuthStore";
 import { Button } from "../ui/button";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Logout = () => {
-  const { signOut } = useAuthStore();
+  const signOut = useAuthStore((s) => s.signOut);
   const navigate = useNavigate();
+
   const handleLogout = async () => {
     try {
       await signOut();
       navigate("/signin");
     } catch (error) {
-      console.error(error);
+      console.error("Lỗi đăng xuất:", error);
+      toast.error("Có lỗi xảy ra khi đăng xuất!");
     }
   };
+
   return <Button onClick={handleLogout}>Đăng xuất</Button>;
 };
+
 export default Logout;
