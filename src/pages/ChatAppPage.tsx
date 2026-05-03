6461de54babf06ff625b9dc811020978aa1d0ef9
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-04 00:52:01 +0700

    refactor: clean up ChatAppPage by removing unused test button and related logic

--- src/pages/ChatAppPage.tsx
+++ src/pages/ChatAppPage.tsx
@@ -1,27 +1,13 @@
-import Logout from "@/components/auth/logout";
-import { Button } from "@/components/ui/button";
-import api from "@/lib/axios";
+import Logout from "@/components/auth/Logout";
 import { useAuthStore } from "@/stores/useAuthStore";
-import { toast } from "sonner";
 
 const ChatAppPage = () => {
   const user = useAuthStore((s) => s.user);
 
-  const handleOnclick = async () => {
-    try {
-      await api.get("/users/test", { withCredentials: true });
-      toast.success("ok");
-    } catch (error) {
-      toast.error("fail");
-      console.error(error);
-    }
-  };
   return (
     <>
       {user?.username}
       <Logout />
-
-      <Button onClick={handleOnclick}>Test</Button>
     </>
   );
 };
