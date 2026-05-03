6530c84a08695c8a86ab6371b7a8825ac84a4ed2
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-03 14:49:49 +0700

    feat: add request interceptor to include access token in axios headers

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,21 @@
+import { useAuthStore } from "@/stores/useAuthStore";
 import axios from "axios"
 
 const api = axios.create({
     baseURL: import.meta.env.MODE === 'development' ? "http://localhost:5001/api" : "/api",
     withCredentials: true, // gui cookie len server
-})
+});
+
+// gan access token vao req header
+api.interceptors.request.use((config) => {
+    const {accessToken} = useAuthStore.getState();
+
+    if (accessToken)
+    {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+});
 
 export default api
