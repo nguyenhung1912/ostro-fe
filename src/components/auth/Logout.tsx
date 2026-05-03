08117ecb331022fba0fe9052b51b1b72654a6451
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-04 00:47:29 +0700

    refactor: simplify logout handling by removing error handling and toast notifications

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,23 +1,16 @@
 import { useAuthStore } from "@/stores/useAuthStore";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
-import { toast } from "sonner";
 
 const Logout = () => {
   const signOut = useAuthStore((s) => s.signOut);
   const navigate = useNavigate();
 
   const handleLogout = async () => {
-    try {
-      await signOut();
-      navigate("/signin");
-    } catch (error) {
-      console.error("Lỗi đăng xuất:", error);
-      toast.error("Có lỗi xảy ra khi đăng xuất!");
-    }
+    await signOut();
+    navigate("/signin");
   };
 
   return <Button onClick={handleLogout}>Đăng xuất</Button>;
 };
-
 export default Logout;
