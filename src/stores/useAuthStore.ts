f3cb317787ef3f1ef9c22c1b062da317d890d258
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-03 16:05:20 +0700

    feat: implement refresh method in useAuthStore for token renewal and user data fetching

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -8,9 +8,13 @@ export const useAuthStore = create<AuthState>((set, get) => ({
   user: null,
   loading: false,
 
-clearState: () => {
-    set({accessToken: null, user: null, loading: false})
-},
+  setAccessToken: (accessToken) => {
+    set({ accessToken });
+  },
+
+  clearState: () => {
+    set({ accessToken: null, user: null, loading: false });
+  },
 
   signUp: async (username, password, email, firstName, lastName) => {
     try {
@@ -35,10 +39,10 @@ clearState: () => {
       set({ loading: true });
 
       const { accessToken } = await authService.signIn(username, password);
-      set({ accessToken });
+      get().setAccessToken(accessToken);
 
       await get().fetchMe();
-      
+
       toast.success("Chào mừng bạn quay lại với Ostro");
     } catch (error) {
       console.error(error);
@@ -50,28 +54,48 @@ clearState: () => {
 
   signOut: async () => {
     try {
-        get().clearState();
-        await authService.signOut();
-        toast.success("Đăng xuất thành công")
+      get().clearState();
+      await authService.signOut();
+      toast.success("Đăng xuất thành công");
     } catch (error) {
-        console.error(error);
-        toast.error("Lỗi xảy ra khi đăng xuất. Hãy thử lại")
+      console.error(error);
+      toast.error("Lỗi xảy ra khi đăng xuất. Hãy thử lại");
     } finally {
-      set({loading: false})
+      set({ loading: false });
     }
   },
 
   fetchMe: async () => {
     try {
-      set({loading: true});
+      set({ loading: true });
       const user = await authService.fetchMe();
-      set({user})
+      set({ user });
     } catch (error) {
       console.error(error);
-      set({user: null, accessToken: null});
-      toast.error("Lỗi xảy ra khi lấy dữ liệu người dùng. Hãy thử lại!")
+      set({ user: null, accessToken: null });
+      toast.error("Lỗi xảy ra khi lấy dữ liệu người dùng. Hãy thử lại!");
     } finally {
-      set({loading: false})
+      set({ loading: false });
     }
-  }
+  },
+
+  refresh: async () => {
+    try {
+      set({ loading: true });
+      const { user, fetchMe, setAccessToken } = get();
+      const accessToken = await authService.refresh();
+
+      setAccessToken(accessToken);
+
+      if (!user) {
+        await fetchMe();
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại!");
+      get().clearState();
+    } finally {
+      set({ loading: false });
+    }
+  },
 }));
