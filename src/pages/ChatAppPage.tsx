914ffb93012064345fdd40e909cf1fa3900b659b
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-03 16:02:21 +0700

    feat: add test button to ChatAppPage for user data retrieval

--- src/pages/ChatAppPage.tsx
+++ src/pages/ChatAppPage.tsx
@@ -1,12 +1,27 @@
 import Logout from "@/components/auth/logout";
+import { Button } from "@/components/ui/button";
+import api from "@/lib/axios";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { toast } from "sonner";
 
 const ChatAppPage = () => {
   const user = useAuthStore((s) => s.user);
+
+  const handleOnclick = async () => {
+    try {
+      await api.get("/users/test", { withCredentials: true });
+      toast.success("ok");
+    } catch (error) {
+      toast.error("fail");
+      console.error(error);
+    }
+  };
   return (
     <>
       {user?.username}
       <Logout />
+
+      <Button onClick={handleOnclick}>Test</Button>
     </>
   );
 };
