b9fc2e3a01f58bac007aebf85239d9c64173648a
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-13 00:57:26 +0700

    feat: add DirectMessageCard component for displaying direct messages

---
+++ src/components/chat/DirectMessageCard.tsx
@@ -0,0 +1,8 @@
+import type { Conversation } from "@/types/chat"
+
+const DirectMessageCard = ({convo}: {convo: Conversation}) => {
+  return (
+    <div>DirectMessageCard</div>
+  )
+}
+export default DirectMessageCard
