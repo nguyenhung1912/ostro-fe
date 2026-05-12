7126b893cb8b3d14fc6c0be40b6e0500c0e3f02a
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-13 00:57:39 +0700

    feat: add GroupChatCard component for displaying group conversations

---
+++ src/components/chat/GroupChatCard.tsx
@@ -0,0 +1,6 @@
+import type { Conversation } from "@/types/chat";
+
+const GroupChatCard = ({ convo }: { convo: Conversation }) => {
+  return <div>GroupChatCard</div>;
+};
+export default GroupChatCard;
