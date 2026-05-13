f0a813d8964704e3dc012a3cffb21cec03ded3b1
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-13 22:36:18 +0700

    feat: implement GroupChatCard component to display group conversations

--- src/components/chat/GroupChatCard.tsx
+++ src/components/chat/GroupChatCard.tsx
@@ -1,6 +1,43 @@
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
 import type { Conversation } from "@/types/chat";
+import ChatCard from "./ChatCard";
 
 const GroupChatCard = ({ convo }: { convo: Conversation }) => {
-  return <div>GroupChatCard</div>;
+  const { user } = useAuthStore();
+  const { activeConversationId, setActiveConversation, messages } =
+    useChatStore();
+
+  if (!user) return null;
+
+  const unreadCount = convo.unreadCounts[user._id];
+  const name = convo.group?.name ?? "";
+  const handleSelectConversation = async (id: string) => {
+    setActiveConversation(id);
+    if (!messages[id]) {
+      // todo: fetch messages
+    }
+  };
+
+  return (
+    <ChatCard
+      convoId={convo._id}
+      name={name}
+      timestamp={
+        convo.lastMessage?.createdAt
+          ? new Date(convo.lastMessage.createdAt)
+          : undefined
+      }
+      isActive={activeConversationId === convo._id}
+      onSelect={handleSelectConversation}
+      unreadCount={unreadCount}
+      leftSection={<>{/* todo: group avatar */}</>}
+      subtitle={
+        <p className="text-sm truncate text-muted-foreground">
+          {convo.participants.length} thành viên
+        </p>
+      }
+    />
+  );
 };
 export default GroupChatCard;
