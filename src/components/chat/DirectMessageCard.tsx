63a8a91b87cfde6e7d4f2ad20c755b78feb8cdcb
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-13 22:36:10 +0700

    feat: implement DirectMessageCard component to display individual conversations

--- src/components/chat/DirectMessageCard.tsx
+++ src/components/chat/DirectMessageCard.tsx
@@ -1,6 +1,61 @@
 import type { Conversation } from "@/types/chat";
+import ChatCard from "./ChatCard";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
+import { cn } from "@/lib/utils";
 
 const DirectMessageCard = ({ convo }: { convo: Conversation }) => {
-  return <ChatCard />;
+  const { user } = useAuthStore();
+  const { activeConversationId, setActiveConversation, messages } =
+    useChatStore();
+
+  if (!user) return null;
+
+  const otherUser = convo.participants.find((p) => p._id !== user._id);
+  if (!otherUser) return null;
+
+  const unreadCount = convo.unreadCounts[user._id];
+  const lastMessage = convo.lastMessage?.content ?? "";
+
+  const handleSelectConversation = async (id: string) => {
+    setActiveConversation(id);
+    if (!messages[id]) {
+      // todo: fetch messages
+    }
+  };
+
+  return (
+    <ChatCard
+      convoId={convo._id}
+      name={otherUser.displayName ?? ""}
+      timestamp={
+        convo.lastMessage?.createdAt
+          ? new Date(convo.lastMessage.createdAt)
+          : undefined
+      }
+      isActive={activeConversationId === convo._id}
+      onSelect={handleSelectConversation}
+      unreadCount={unreadCount}
+      leftSection={
+        <>
+          {/* todo: user avatar */}
+          {/* todo: status badge*/}
+          {/* todo: unread count */}
+        </>
+      }
+      subtitle={
+        <p
+          className={cn(
+            "text-sm truncate",
+            unreadCount > 0
+              ? "font-medium text-foreground"
+              : "text-muted-foreground",
+          )}
+        >
+          {lastMessage}
+        </p>
+      }
+    />
+  );
 };
 export default DirectMessageCard;
