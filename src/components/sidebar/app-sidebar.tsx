a33ce3a3e06810fb18e6772313f7b9d3d938e243
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-14 23:10:12 +0700

    feat: add scrollbar styling to SidebarContent for improved usability

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -57,7 +57,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </SidebarMenu>
       </SidebarHeader>
       {/* Content */}
-      <SidebarContent>
+      <SidebarContent className="beautiful-scrollbar">
         {/* New Chat */}
         <SidebarGroup>
           <SidebarGroupContent>
