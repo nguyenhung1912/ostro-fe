3a9cdb89da2a1eba59f83d06531133caafc279f4
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-14 22:58:41 +0700

    feat: implement ChatWindowBody component for chat interface display

---
+++ src/components/chat/ChatWindowBody.tsx
@@ -0,0 +1,38 @@
+import { useChatStore } from "@/stores/useChatStore";
+import ChatWelcomeScreen from "./ChatWelcomeScreen";
+
+const ChatWindowBody = () => {
+  const {
+    activeConversationId,
+    conversations,
+    messages: allMessages,
+  } = useChatStore();
+
+  const messages = allMessages[activeConversationId!]?.items ?? [];
+  const selectedConvo = conversations.find(
+    (c) => c._id === activeConversationId,
+  );
+
+  if (!selectedConvo) {
+    return <ChatWelcomeScreen />;
+  }
+
+  if (!messages?.length) {
+    return (
+      <div className="flex h-full items-center justify-center text-muted fore-ground">
+        Chưa có tin nhắn nào trong cuộc trò chuyện này.
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4 bg-primary-foreground h-full flex flex-col overflow-hidden">
+      <div className="flex flex-col overflow-y-auto overflow-x-hidden beautiful-scrollbar">
+        {messages.map((message) => (
+          <>{message.content}</>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default ChatWindowBody;
