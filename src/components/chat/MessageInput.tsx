a437b00d32eff9a00a44ca9be3663009ee550121
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-15 12:38:21 +0700

    feat: implement message sending functionality with direct and group options

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -1,16 +1,45 @@
 import { useAuthStore } from "@/stores/useAuthStore";
 import type { Conversation } from "@/types/chat";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { ImagePlus, Send } from "lucide-react";
 import { Input } from "../ui/input";
 import EmojiPicker from "./EmojiPicker";
+import { useChatStore } from "@/stores/useChatStore";
+import { toast } from "sonner";
 
 const MessageInput = ({ selectedConvo }: { selectedConvo: Conversation }) => {
   const { user } = useAuthStore();
+  const { sendDirectMessage, sendGroupMessage } = useChatStore();
   const [value, setValue] = useState("");
 
   if (!user) return;
+
+  const sendMessage = async () => {
+    if (!value.trim()) return;
+
+    try {
+      if (selectedConvo.type === "direct") {
+        const participants = selectedConvo.participants;
+        const otherUser = participants.filter((p) => p._id !== user._id)[0];
+        await sendDirectMessage(otherUser._id, value);
+      } else {
+        await sendGroupMessage(selectedConvo._id, value);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Lỗi xảy ra khi gửi tin nhắn. Bạn hãy thử lại!");
+    } finally {
+      setValue("");
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      sendMessage();
+    }
+  };
 
   return (
     <div className="flex items-center gap-2 p-3 min-h-[56px] bg-background">
@@ -24,6 +53,7 @@ const MessageInput = ({ selectedConvo }: { selectedConvo: Conversation }) => {
 
       <div className="flex-1 relative">
         <Input
+          onKeyDown={handleKeyPress}
           value={value}
           onChange={(e) => setValue(e.target.value)}
           placeholder="Soạn tin nhắn..."
@@ -45,6 +75,7 @@ const MessageInput = ({ selectedConvo }: { selectedConvo: Conversation }) => {
         </div>
       </div>
       <Button
+        onClick={sendMessage}
         className="bg-gradient-chat hover:shadow-glow transition-smooth hover:scale-105"
         disabled={!value.trim()}
       >
