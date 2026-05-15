466f584ac028044f0f4f5154ba5b0289e18d04e6
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-15 13:44:47 +0700

    feat: implement socket store for real-time messaging and online user tracking

---
+++ src/stores/useSocketStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { io, type Socket } from "socket.io-client";
+import { useAuthStore } from "./useAuthStore";
+import type { SocketState } from "@/types/store";
+
+const baseUrl = import.meta.env.VITE_SOCKET_URL;
+
+export const useSocketStore = create<SocketState>((set, get) => ({
+  socket: null,
+  onlineUsers: [];
+  connectSocket: () => {
+    const accessToken = useAuthStore.getState().accessToken;
+    const existingSocket = get().socket;
+
+    if (existingSocket) return;
+
+    const socket: Socket = io(baseUrl, {
+      auth: { token: accessToken },
+      transports: ["websocket"],
+    });
+
+    set({ socket });
+
+    socket.on("connect", () => {
+      console.log("Đã kết nối với socket");
+    });
+
+    // online users
+    socket.on("online-users", (userIds) => {
+        set({onlineUsers: userIds});
+    })
+  },
+
+  disconnectSocket: () => {
+    const socket = get().socket;
+    if (socket) {
+      socket.disconnect();
+      set({ socket: null });
+    }
+  },
+}));
