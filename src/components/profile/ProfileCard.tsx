b3b1bc077e30cf5fe99f7d800c63fbdb80ebeeaa
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-17 16:10:01 +0700

    wip

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,9 +1,10 @@
 import type { User } from "@/types/user";
 import { Card, CardContent } from "../ui/card";
 import UserAvatar from "../chat/UserAvatar";
-import { Badge } from "lucide-react";
+import { Badge } from "../ui/badge";
 import { cn } from "@/lib/utils";
 import { useSocketStore } from "@/stores/useSocketStore";
+import AvatarUploader from "./AvatarUploader";
 
 interface ProfileCardProps {
   user: User | null;
@@ -11,17 +12,18 @@ interface ProfileCardProps {
 
 const ProfileCard = ({ user }: ProfileCardProps) => {
   const { onlineUsers } = useSocketStore();
-
   if (!user) return;
 
-  const displayBio = user.bio || "Will code for food";
+  if (!user.bio) {
+    user.bio = "Will code for food 💻";
+  }
 
   const isOnline = onlineUsers.includes(user._id) ? true : false;
 
   return (
-    <Card className="overflow-hidden p-0 h-52 bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <Card className="overflow-hidden p-0 h-52 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <CardContent className="mt-20 pb-8 flex flex-col sm:flex-row items-center sm:items-end gap-6">
-        <div>
+        <div className="relative">
           <UserAvatar
             type="profile"
             name={user.displayName}
@@ -29,7 +31,7 @@ const ProfileCard = ({ user }: ProfileCardProps) => {
             className="ring-4 ring-white shadow-lg"
           />
 
-          {/* upload avatar */}
+          <AvatarUploader />
         </div>
 
         {/* user info */}
@@ -37,13 +39,15 @@ const ProfileCard = ({ user }: ProfileCardProps) => {
           <h1 className="text-2xl font-semibold tracking-tight text-white">
             {user.displayName}
           </h1>
-          <p className="text-white/70 text-sm mt-2 max-w-lg line-clamp-2">
-            {displayBio}
-          </p>
+
+          {user.bio && (
+            <p className="text-white/70 text-sm mt-2 max-w-lg line-clamp-2">
+              {user.bio}
+            </p>
+          )}
         </div>
 
         {/* status */}
-
         <Badge
           className={cn(
             "flex items-center gap-1 capitalize",
@@ -54,14 +58,16 @@ const ProfileCard = ({ user }: ProfileCardProps) => {
         >
           <div
             className={cn(
-              "size-2 rounded-full ",
+              "size-2 rounded-full",
               isOnline ? "bg-green-500 animate-pulse" : "bg-slate-500",
             )}
           />
+
           {isOnline ? "online" : "offline"}
         </Badge>
       </CardContent>
     </Card>
   );
 };
+
 export default ProfileCard;
