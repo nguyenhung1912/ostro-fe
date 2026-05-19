16b9fdc79c1b61c94abd2c5975360fba7e402aee
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-19 18:34:01 +0700

    refactor: adjust positioning and size of StatusBadge for improved layout

--- src/components/chat/StatusBadge.tsx
+++ src/components/chat/StatusBadge.tsx
@@ -4,7 +4,7 @@ const StatusBadge = ({ status }: { status: "online" | "offline" }) => {
   return (
     <div
       className={cn(
-        "absolute -bottom-1 -right-1 size-4 z-10",
+        "absolute bottom-0 right-0 size-3.5 z-10",
         status === "online" && "status-online",
         status === "offline" && "status-offline",
       )}
