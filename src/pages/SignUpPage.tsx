bce3599baca84dc53a7f564987287315603f8664
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-20 01:01:28 +0700

    refactor: standardize max-width for SignInPage and SignUpPage components

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -3,10 +3,9 @@ import { SignUpForm } from "@/components/auth/signup-form";
 const SignUpPage = () => {
   return (
     <>
-      <div className="fixed inset-0 overflow-hidden pointer-events-none z-[-1] bg-background">
-      </div>
+      <div className="fixed inset-0 overflow-hidden pointer-events-none z-[-1] bg-background"></div>
       <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10 relative z-0">
-        <div className="w-full max-w-sm md:max-w-4xl">
+        <div className="w-full max-w-sm md:max-w-md">
           <SignUpForm />
         </div>
       </div>
